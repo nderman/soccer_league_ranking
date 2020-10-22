106bab66e34d03fee1fee2a0ff60604fac3e84c1
Neal <neal.derman@hetzner.co.za>
2020-10-22 21:10:32 +0200

    happy day specs passing

--- classes/match.rb
+++ classes/match.rb
@@ -12,5 +12,8 @@ class Match
   end
 
   def league_points
+    return [{ team: @team1, pts: 1 }, { team: @team2, pts: 1 }] if @score1 == @score2
+    return [{ team: @team1, pts: 3 }, { team: @team2, pts: 0 }] if @score1 > @score2
+    return [{ team: @team1, pts: 0 }, { team: @team2, pts: 3 }] if @score1 < @score2
   end
 end
